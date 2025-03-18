5f92d3e1b64a5622d493936d5cebf5c4deedfcb7
Sammie-code-code <sammiebamz694@gmail.com>
2025-03-18 15:51:35 +0100

    PrintMethods

---
+++ PrintMethods.java
@@ -0,0 +1,18 @@
+public class PrintMethods {
+    public static void main(String[] args) {
+	   //Using the print()
+	   System.out.print("\"Learning java is fun\"\n");
+	   System.out.print(" Hey come lets learn java");
+	   
+	   //Using the println()
+	   System.out.println();
+	   System.out.println(" I want to tell you that java is different from Java script");
+	   System.out.println("\t this is the indent");
+	   
+	   //Using printf
+	   System.out.printf("my name is %s and i love %s", "Samuel", "Java");
+	   System.out.printf("\n i live at number %d road %n", 1 );
+	   System.out.printf("\n I bought PS5 at %.1f Naira %n", 181.90);
+	   System.out.printf("\n I bought java with %c%f", '$', 32.486);
+	}
+}
