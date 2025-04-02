aa7a84d6f37c7624dfb6f4ec54947a6448a8ebc8
Sammie-code-code <sammiebamz694@gmail.com>
2025-04-02 14:17:16 +0100

    Chapter 3.docx

---
+++ ForLoopStatement.java
@@ -0,0 +1,15 @@
+public class ForLoopStatement{
+	public static void main(String[] args){
+	
+	    int num = 50;
+	    String name = "Samuel";
+	    
+		for(int i = 1; i <= 10; i++){
+			System.out.printf("%d%n",num);
+		}
+		
+		for(int i = 1; i <= 10; i++){
+			System.out.printf("%s%n",name);
+		}
+	}
+}
